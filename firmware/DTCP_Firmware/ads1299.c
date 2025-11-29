d8bd0ff05e5b6689b4e033b148bf21c298094062
ngaask <stavskyidmytro@ufl.edu>
2025-11-28 19:09:25 -0500

    Added the declarations for the future structure

--- firmware/DTCP_Firmware/ads1299.c
+++ firmware/DTCP_Firmware/ads1299.c
@@ -1,10 +1,14 @@
 #include "ads1299.h"
 
-void ADS1299_init(void) {
+void SPI_init() {
 
 }
 
-void ADS_1299_write_register(uint8_t reg_addr, uint8_t num_regs, uint8_t data) {
+void ADS1299_init() {
+	SPI_init();
+}
+
+void ADS_1299_write_registers(uint8_t reg_addr, uint8_t num_regs, uint8_t data) {
         uint8_t data_buffer[3] = {reg_addr, num_regs - 1, data};
         DL_GPIO_clearPins(GPIOA, DL_GPIO_PIN_23);
         DL_SPI_transmitDataBlocking8(SPI0, data_buffer[0]);
--- firmware/DTCP_Firmware/ads1299.h
+++ firmware/DTCP_Firmware/ads1299.h
@@ -1,6 +1,7 @@
 #include "ti/driverlib/dl_spi.h"
 #include "ti/driverlib/dl_gpio.h"
 
+void SPI_init(void);
 void ADS_1299_init(void);
-void ADS_1299_write_register(uint8_t reg_addr, uint8_t num_regs, uint8_t data);
+void ADS_1299_write_registers(uint8_t reg_addr, uint8_t num_regs, uint8_t data);
 void ADS_1299_read_register(void);
