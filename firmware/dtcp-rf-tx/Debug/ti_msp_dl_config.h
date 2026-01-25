23f4215c4376fe1998dfcaa1befe2cc66486d929
jdados <jeremiasdados@ufl.edu>
2026-01-25 11:46:41 -0500

    Added the firmware for RF transmission, for now as a separate project

---
+++ firmware/dtcp-rf-tx/Debug/ti_msp_dl_config.h
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2023, Texas Instruments Incorporated - http://www.ti.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ *  ============ ti_msp_dl_config.h =============
+ *  Configured MSPM0 DriverLib module declarations
+ *
+ *  DO NOT EDIT - This file is generated for the MSPM0C110X
+ *  by the SysConfig tool.
+ */
+#ifndef ti_msp_dl_config_h
+#define ti_msp_dl_config_h
+
+#define CONFIG_MSPM0C110X
+#define CONFIG_MSPM0C1104
+
+#if defined(__ti_version__) || defined(__TI_COMPILER_VERSION__)
+#define SYSCONFIG_WEAK __attribute__((weak))
+#elif defined(__IAR_SYSTEMS_ICC__)
+#define SYSCONFIG_WEAK __weak
+#elif defined(__GNUC__)
+#define SYSCONFIG_WEAK __attribute__((weak))
+#endif
+
+#include <ti/devices/msp/msp.h>
+#include <ti/driverlib/driverlib.h>
+#include <ti/driverlib/m0p/dl_core.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ *  ======== SYSCFG_DL_init ========
+ *  Perform all required MSP DL initialization
+ *
+ *  This function should be called once at a point before any use of
+ *  MSP DL.
+ */
+
+
+/* clang-format off */
+
+#define POWER_STARTUP_DELAY                                                (16)
+
+
+
+#define CPUCLK_FREQ                                                     24000000
+
+
+
+
+/* Port definition for Pin Group PA */
+#define PA_PORT                                                          (GPIOA)
+
+/* Defines for EN: GPIOA.6 with pinCMx 7 on package pin 10 */
+#define PA_EN_PIN                                                (DL_GPIO_PIN_6)
+#define PA_EN_IOMUX                                               (IOMUX_PINCM7)
+/* Port definition for Pin Group DDS */
+#define DDS_PORT                                                         (GPIOA)
+
+/* Defines for WCLK: GPIOA.23 with pinCMx 24 on package pin 18 */
+#define DDS_WCLK_PIN                                            (DL_GPIO_PIN_23)
+#define DDS_WCLK_IOMUX                                           (IOMUX_PINCM24)
+/* Defines for D7: GPIOA.25 with pinCMx 26 on package pin 20 */
+#define DDS_D7_PIN                                              (DL_GPIO_PIN_25)
+#define DDS_D7_IOMUX                                             (IOMUX_PINCM26)
+/* Defines for RST: GPIOA.24 with pinCMx 25 on package pin 19 */
+#define DDS_RST_PIN                                             (DL_GPIO_PIN_24)
+#define DDS_RST_IOMUX                                            (IOMUX_PINCM25)
+/* Defines for FREQ_UPD: GPIOA.22 with pinCMx 23 on package pin 17 */
+#define DDS_FREQ_UPD_PIN                                        (DL_GPIO_PIN_22)
+#define DDS_FREQ_UPD_IOMUX                                       (IOMUX_PINCM23)
+/* Defines for DDS_EN: GPIOA.0 with pinCMx 1 on package pin 4 */
+#define DDS_DDS_EN_PIN                                           (DL_GPIO_PIN_0)
+#define DDS_DDS_EN_IOMUX                                          (IOMUX_PINCM1)
+
+
+/* clang-format on */
+
+void SYSCFG_DL_init(void);
+void SYSCFG_DL_initPower(void);
+void SYSCFG_DL_GPIO_init(void);
+void SYSCFG_DL_SYSCTL_init(void);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ti_msp_dl_config_h */
---
+++ firmware/dtcp-rf-tx/gpio_toggle_output.c
@@ -0,0 +1,134 @@
+#include "ti_msp_dl_config.h"
+#include <math.h>
+
+/* This results in approximately 0.5s of delay assuming 24MHz CPU_CLK */
+#define DDS_SYSCLK_FREQ (100000000)
+#define MCU_CLK_FREQ (24000000)
+#define DDS_WCLK_FREQ (1000000)
+#define DDS_WCLK_T_US (10)
+
+/* Wrapper API */
+void wait_us(uint32_t t){
+    delay_cycles((MCU_CLK_FREQ/1000000)*t);
+}
+
+void pin_init(){
+    //DL_GPIO_clearPins(DDS_PORT, DDS_WCLK_PIN | DDS_FQ_UD_PIN | DDS_RST_PIN);
+    DL_GPIO_setPins(DDS_PORT, DDS_DDS_EN_PIN);
+    //DL_GPIO_clearPins(PA_PORT, PA_EN_PIN);
+}
+
+void dds_reset(){
+    /*Pulse RST for at least 5 clock cycles*/
+    DL_GPIO_clearPins(DDS_PORT, DDS_WCLK_PIN);
+    DL_GPIO_setPins(DDS_PORT, DDS_RST_PIN);
+    wait_us(DDS_WCLK_T_US/2);
+
+    for(int i = 0; i < 10; i++){
+        wait_us(DDS_WCLK_T_US/2);
+        DL_GPIO_togglePins(DDS_PORT, DDS_WCLK_PIN);
+        wait_us(DDS_WCLK_T_US/2);
+        DL_GPIO_togglePins(DDS_PORT, DDS_WCLK_PIN);
+    }
+
+    DL_GPIO_clearPins(DDS_PORT, DDS_RST_PIN);
+}
+
+void dds_serial_load_en(){
+    /* Set W[2:0] to 011 */
+    // DL_GPIO_setPins(DDS_PORT, DDS_D0_PIN | DDS_D1_PIN);
+    // DL_GPIO_clearPins(DDS_PORT, DDS_D2_PIN);
+    // wait_us(DDS_WCLK_T_US*10);
+
+    /* Pulse WCLK */
+    DL_GPIO_setPins(DDS_PORT, DDS_WCLK_PIN);
+    wait_us(DDS_WCLK_T_US);
+    DL_GPIO_clearPins(DDS_PORT, DDS_WCLK_PIN);
+    wait_us(DDS_WCLK_T_US*4);
+
+
+    /* Pulse FQ_UD */
+    wait_us(DDS_WCLK_T_US);
+    DL_GPIO_setPins(DDS_PORT, DDS_FREQ_UPD_PIN);
+    wait_us(DDS_WCLK_T_US);
+    DL_GPIO_clearPins(DDS_PORT, DDS_FREQ_UPD_PIN);
+    wait_us(DDS_WCLK_T_US);
+}
+
+void dds_serial_data_tx(uint32_t dword){
+    /* Send the frequency info */
+    uint32_t dword_temp = dword;
+    for(int i = 0; i < 32; i++){
+        /* Setting W0-W31 */
+        if((dword_temp & 0x1) == 0x1){
+            DL_GPIO_setPins(DDS_PORT, DDS_D7_PIN);
+        } else {
+            DL_GPIO_clearPins(DDS_PORT, DDS_D7_PIN);
+        }
+
+        /* Clock toggling */
+        if(i == 0){
+            wait_us(DDS_WCLK_T_US/2);
+            DL_GPIO_setPins(DDS_PORT, DDS_WCLK_PIN);
+            wait_us(DDS_WCLK_T_US/2);
+            DL_GPIO_clearPins(DDS_PORT, DDS_WCLK_PIN);
+        } else {
+            wait_us(DDS_WCLK_T_US/2);
+            DL_GPIO_togglePins(DDS_PORT, DDS_WCLK_PIN);
+            wait_us(DDS_WCLK_T_US/2);
+            DL_GPIO_togglePins(DDS_PORT, DDS_WCLK_PIN);
+        }
+
+        /* Look at the next bit */
+        dword_temp = dword_temp>>1;
+    }
+
+    /* W32 and W33 are control bits, always set to 0 */
+    /* The rest of the bits should be 0 too */
+    DL_GPIO_clearPins(DDS_PORT, DDS_D7_PIN);
+    for(int i = 0; i < 8; i++){
+        wait_us(DDS_WCLK_T_US/2);
+        DL_GPIO_togglePins(DDS_PORT, DDS_WCLK_PIN);
+        wait_us(DDS_WCLK_T_US/2);
+        DL_GPIO_togglePins(DDS_PORT, DDS_WCLK_PIN);
+    }
+
+    wait_us(DDS_WCLK_T_US);
+    /*Pulse FQ_UD at the end */
+    DL_GPIO_setPins(DDS_PORT, DDS_FREQ_UPD_PIN);
+    wait_us(DDS_WCLK_T_US);
+    DL_GPIO_clearPins(DDS_PORT, DDS_FREQ_UPD_PIN);
+    wait_us(DDS_WCLK_T_US);
+}
+
+#define F_OUT_DDS 25000000
+
+int main(void)
+{
+    /* Power on GPIO, initialize pins as digital outputs */
+    SYSCFG_DL_init();
+
+    /* Initialize the GPIO for the DDS interface */
+    pin_init();
+
+    /* Reset the DDS */
+    dds_reset();
+
+    /* Initialize DDS in serial mode */
+    dds_serial_load_en();
+
+    /* Calculate the DDS output frequency */
+    double f_ratio = (double)F_OUT_DDS/(double)DDS_SYSCLK_FREQ;
+    uint32_t freq_dword = (uint32_t)(f_ratio*pow(2,32));
+
+    dds_serial_data_tx(freq_dword);
+    //DL_GPIO_setPins(PA_PORT, PA_EN_PIN);
+    while (1) {
+        // DL_GPIO_setPins(PA_PORT, PA_EN_PIN);
+        // dds_serial_data_tx(freq_dword);
+        // wait_us(10000);
+        // dds_reset();
+        // DL_GPIO_clearPins(PA_PORT, PA_EN_PIN);
+        // wait_us(990000);
+    }
+}
