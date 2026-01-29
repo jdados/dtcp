aec8abe4434daca8866627a93df9229518e9435b
Dmytro Stavskyi <stavskyidmytro@ufl.edu>
2026-01-29 17:53:26 -0500

    Can measure the internally generated signals; the measured signal it is correctly a square wave but I am not yet sure about the amplitude

--- firmware/DTCP_Firmware/main.c
+++ firmware/DTCP_Firmware/main.c
@@ -38,41 +38,41 @@ int main(void) {
     ADS1299_transmit_cmd(SDATAC_cmd);
 
     /* Write to CONFIG1  - CHN4 registers */
-    // DL_GPIO_clearPins(GPIO_A_PORT, GPIO_A_CS_PIN);
-    // DL_SPI_transmitDataBlocking8(SPI0, 0x40 | 1);
-    // DL_SPI_receiveData8(SPI_0_INST);
-    // DL_SPI_transmitDataBlocking8(SPI0, 8 - 1);
-    // DL_SPI_receiveData8(SPI_0_INST);
-    // /* DAISY_EN = 1, CLK_EN = 0, Output data rate = 1 kSPS */
-    // DL_SPI_transmitDataBlocking8(SPI0, 0b11010100);
-    // // DL_SPI_transmitDataBlocking8(SPI0, 0x96);
-    // DL_SPI_receiveData8(SPI_0_INST);
-    // /* CONFIG 2*/
-    // DL_SPI_transmitDataBlocking8(SPI0, 0b11000000);
-    // // DL_SPI_transmitDataBlocking8(SPI0, 0xC0);
-    // DL_SPI_receiveData8(SPI_0_INST);
-    // /* CONFIG 3 */
-    // DL_SPI_transmitDataBlocking8(SPI0, 0xE0);
-    // DL_SPI_receiveData8(SPI_0_INST);
-    // /* default values */
-    // DL_SPI_transmitDataBlocking8(SPI0, 0x00);
-    // DL_SPI_receiveData8(SPI_0_INST);
-    // /* CH 1*/
-    // DL_SPI_transmitDataBlocking8(SPI0, 0b01100001);
-    // // DL_SPI_transmitDataBlocking8(SPI0, 0x01);
-    // DL_SPI_receiveData8(SPI_0_INST);
-    // /* CH 2*/
-    // DL_SPI_transmitDataBlocking8(SPI0, 0b01100001);
-    // DL_SPI_receiveData8(SPI_0_INST);
-    // /* CH 3 */
-    // DL_SPI_transmitDataBlocking8(SPI0, 0x81);
-    // DL_SPI_receiveData8(SPI_0_INST);
-    // /* CH 4 */
-    // DL_SPI_transmitDataBlocking8(SPI0, 0x81);
-    // DL_SPI_receiveData8(SPI_0_INST);
-    // delay_cycles(50);
-    // DL_GPIO_setPins(GPIO_A_PORT, GPIO_A_CS_PIN);
-    // delay_cycles(2e3);
+    DL_GPIO_clearPins(GPIO_A_PORT, GPIO_A_CS_PIN);
+    DL_SPI_transmitDataBlocking8(SPI0, 0x40 | 1);
+    DL_SPI_receiveData8(SPI_0_INST);
+    DL_SPI_transmitDataBlocking8(SPI0, 8 - 1);
+    DL_SPI_receiveData8(SPI_0_INST);
+    /* DAISY_EN = 1, CLK_EN = 0, Output data rate = 1 kSPS */
+    DL_SPI_transmitDataBlocking8(SPI0, 0b11010100);
+    // DL_SPI_transmitDataBlocking8(SPI0, 0x96);
+    DL_SPI_receiveData8(SPI_0_INST);
+    /* CONFIG 2*/
+    DL_SPI_transmitDataBlocking8(SPI0, 0b11010000);
+    // DL_SPI_transmitDataBlocking8(SPI0, 0xC0);
+    DL_SPI_receiveData8(SPI_0_INST);
+    /* CONFIG 3 */
+    DL_SPI_transmitDataBlocking8(SPI0, 0xE0);
+    DL_SPI_receiveData8(SPI_0_INST);
+    /* default values */
+    DL_SPI_transmitDataBlocking8(SPI0, 0x00);
+    DL_SPI_receiveData8(SPI_0_INST);
+    /* CH 1*/
+    DL_SPI_transmitDataBlocking8(SPI0, 0b01100101);
+    // DL_SPI_transmitDataBlocking8(SPI0, 0x01);
+    DL_SPI_receiveData8(SPI_0_INST);
+    /* CH 2*/
+    DL_SPI_transmitDataBlocking8(SPI0, 0b01100001);
+    DL_SPI_receiveData8(SPI_0_INST);
+    /* CH 3 */
+    DL_SPI_transmitDataBlocking8(SPI0, 0x81);
+    DL_SPI_receiveData8(SPI_0_INST);
+    /* CH 4 */
+    DL_SPI_transmitDataBlocking8(SPI0, 0x81);
+    DL_SPI_receiveData8(SPI_0_INST);
+    delay_cycles(50);
+    DL_GPIO_setPins(GPIO_A_PORT, GPIO_A_CS_PIN);
+    delay_cycles(2e3);
 
     /* Single-shot */
     // DL_GPIO_clearPins(GPIO_A_PORT, GPIO_A_CS_PIN);
@@ -93,7 +93,7 @@ int main(void) {
 
     // __BKPT(0);
     /* Cannot read from or write to registers in this mode */
-    // ADS1299_transmit_cmd(RDATAC_cmd);
+    ADS1299_transmit_cmd(RDATAC_cmd);
     // DL_GPIO_clearPins(GPIO_A_PORT, GPIO_A_CS_PIN);
     // DL_SPI_transmitDataBlocking8(SPI0, RDATAC_cmd);
     // delay_cycles(1e3);
@@ -108,39 +108,39 @@ int main(void) {
     const float LSB = 2.235e-8;
 
     while (1) { 
-        uint8_t data = ADS1299_read_registers(1, 1);
-        uint8_t data_2 = ADS1299_read_registers(2, 1);
-        __BKPT(0);
+        // uint8_t data = ADS1299_read_registers(1, 1);
+        // uint8_t data_2 = ADS1299_read_registers(2, 1);
+        // __BKPT(0);
 
         // use "XDS110 Class Application/User UART" COM port
 
-        // uint8_t val = 0;
-        // val = DL_GPIO_readPins(GPIO_A_PORT, GPIO_A_DRDY_PIN);
-        // if (val == 0) {
-        //     DL_GPIO_clearPins(GPIO_A_PORT, GPIO_A_CS_PIN);
-        //     // DL_SPI_transmitDataBlocking8(SPI0, RDATA_cmd);
-        //     // DL_SPI_receiveData8(SPI_0_INST);
-        //     for (uint8_t i = 0; i < 15; ++i) {
-        //         DL_SPI_transmitDataBlocking8(SPI0, 0x00);
-        //         data[i] = DL_SPI_receiveData8(SPI_0_INST);
-        //     }
-        //     delay_cycles(50);
-        //     DL_GPIO_setPins(GPIO_A_PORT, GPIO_A_CS_PIN);
+        uint8_t val = 0;
+        val = DL_GPIO_readPins(GPIO_A_PORT, GPIO_A_DRDY_PIN);
+        if (val == 0) {
+            DL_GPIO_clearPins(GPIO_A_PORT, GPIO_A_CS_PIN);
+            // DL_SPI_transmitDataBlocking8(SPI0, RDATA_cmd);
+            // DL_SPI_receiveData8(SPI_0_INST);
+            for (uint8_t i = 0; i < 15; ++i) {
+                DL_SPI_transmitDataBlocking8(SPI0, 0x00);
+                data[i] = DL_SPI_receiveData8(SPI_0_INST);
+            }
+            delay_cycles(50);
+            DL_GPIO_setPins(GPIO_A_PORT, GPIO_A_CS_PIN);
 
-        //     // if (index == 10) index = 0; 
-        //     // channel_1_data[index] = ((int32_t)data[3] << 16) | ((int32_t)data[4] << 8) | ((int32_t)data[5]);
-        //     channel_1_data = ((int32_t)data[3] << 16) | ((int32_t)data[4] << 8) | ((int32_t)data[5]);
-        //     /* sign extend */
-        //     // if (channel_1_data[index] & 0x800000) channel_1_data[index] |= 0xFF000000;
-        //     if (channel_1_data & 0x800000) channel_1_data |= 0xFF000000;
-        //     // if (channel_1_data[index] & 0x800000) channel_1_data[index] -= 0x1000000;
-        //     // channel_1_data[index] = channel_1_data[index] * 4.5 / (2e23 * 1);
-        //     // channel_1_data[index] = (channel_1_data[index] - 0x800000) / (4.5 / (2e23 - 1));
-        //     // channel_1_data[index] = channel_1_data[index] * 4.5 / (2e23 - 1);
-        //     // voltage[index] = (float)channel_1_data[index] * LSB * 1e3f;
-        //     voltage = (float)channel_1_data * LSB * 1e3f;
-        //     // ++index;
-        // }
+            // if (index == 10) index = 0; 
+            // channel_1_data[index] = ((int32_t)data[3] << 16) | ((int32_t)data[4] << 8) | ((int32_t)data[5]);
+            channel_1_data = ((int32_t)data[3] << 16) | ((int32_t)data[4] << 8) | ((int32_t)data[5]);
+            /* sign extend */
+            // if (channel_1_data[index] & 0x800000) channel_1_data[index] |= 0xFF000000;
+            if (channel_1_data & 0x800000) channel_1_data |= 0xFF000000;
+            // if (channel_1_data[index] & 0x800000) channel_1_data[index] -= 0x1000000;
+            // channel_1_data[index] = channel_1_data[index] * 4.5 / (2e23 * 1);
+            // channel_1_data[index] = (channel_1_data[index] - 0x800000) / (4.5 / (2e23 - 1));
+            // channel_1_data[index] = channel_1_data[index] * 4.5 / (2e23 - 1);
+            // voltage[index] = (float)channel_1_data[index] * LSB * 1e3f;
+            voltage = (float)channel_1_data * LSB * 1e3f;
+            // ++index;
+        }
 
         // uint8_t data = ADS1299_read_registers(1, 1);
         // DL_GPIO_clearPins(GPIO_A_PORT, GPIO_A_CS_PIN);
