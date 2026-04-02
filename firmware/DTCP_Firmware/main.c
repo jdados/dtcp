bd3036f349a31fbe1baee71679cc2f962bb9175c
Dmytro Stavskyi <stavskyidmytro@ufl.edu>
2026-04-02 18:22:50 -0400

    Added code for moving average filter

--- firmware/DTCP_Firmware/main.c
+++ firmware/DTCP_Firmware/main.c
@@ -6,8 +6,9 @@
 #include "uart.h"
 
 volatile float voltage;
-float voltages[128];
+float voltages[100];
 uint8_t count;
+float sum;
 
 /* small printf implementation */
 
@@ -28,34 +29,34 @@ int main(void) {
     // NVIC_EnableIRQ(GPIOA_INT_IRQn);
 
     count = 0;
+    sum = 0;
+    for (uint8_t i = 0; i < 100; ++i) voltages[i] = 0;
 
     while (1) { 
         uint8_t val = 0;
         val = DL_GPIO_readPins(GPIO_A_PORT, GPIO_A_DRDY_PIN);
         if (val == 0) {
-            voltage = ADS1299_read_data_channel_1();
-
-            // if (voltage > 1.0f) {
-            //     delay_ms(55);
-
-            // }
+            voltage = ADS1299_read_data_channel_2();
             
+            /* Remove offset */
+            // sum -= voltages[count];
+            // sum += voltage;
+            // voltages[count] = voltage;
+            // count = (count + 1) % 100;
+            // float average = sum / 100;
+
+            // if (fabsf(voltage) - fabsf(average) > 5.0f) {
+            //     DL_GPIO_clearPins(GPIO_A_PORT, GPIO_A_RED_22_PIN);
+            // }
+
             UART_transmit_voltage_binary(voltage);
 
-            if (count == 128) {
-                /* Remove offset */
-                // int sum = 0;
-                // for (uint8_t i = 0; i < 128; ++i) sum += voltages[i];
-                // int average = sum / 128;
-                // for (uint8_t i = 0; i < 128; ++i) voltages[i] -= average;
-                count = 0;
-            }
-            // voltages[count] = voltage;
-            ++count;
         }
     }
 }
 
+
+/* Have a FIFO that takes the samples */
 void GPIOA_IRQHandler(void) {
     uint32_t int_status = DL_GPIO_getPendingInterrupt(GPIOA);
     if (int_status & GPIO_A_DRDY_IOMUX) {
@@ -63,6 +64,7 @@ void GPIOA_IRQHandler(void) {
         // voltages[count] = voltage;
 
         // if (voltage > 1.0f) {
+                /* Timer ? */
         //     delay_ms(55);
         // }
         
